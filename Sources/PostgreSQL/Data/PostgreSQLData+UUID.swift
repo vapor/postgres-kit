d9640f95ddd537a180008a5c6d81daf7c57a6adc
Tanner Nelson <me@tanner.xyz>
2018-01-19 16:02:24 -0500

    point type

---
+++ Sources/PostgreSQL/Data/PostgreSQLData+UUID.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+extension UUID: PostgreSQLDataCustomConvertible {
+    /// See `PostgreSQLDataCustomConvertible.preferredDataType`
+    public static var preferredDataType: PostgreSQLDataType? { return .uuid }
+
+    /// See `PostgreSQLDataCustomConvertible.convertFromPostgreSQLData(_:)`
+    public static func convertFromPostgreSQLData(_ data: PostgreSQLData) throws -> UUID {
+        guard let value = data.data else {
+            throw PostgreSQLError(identifier: "data", reason: "Could not decode UUID from `null` data.")
+        }
+        switch data.type {
+        case .uuid:
+            switch data.format {
+            case .text:
+                let string = try value.makeString()
+                guard let uuid = UUID(uuidString: string) else {
+                    throw PostgreSQLError(identifier: "uuid", reason: "Could not decode UUID from string: \(string)")
+                }
+                return uuid
+            case .binary: return UUID(uuid: value.unsafeCast())
+            }
+        default: throw PostgreSQLError(identifier: "uuid", reason: "Could not decode UUID from data type: \(data.type)")
+        }
+    }
+
+    /// See `PostgreSQLDataCustomConvertible.convertToPostgreSQLData()`
+    public func convertToPostgreSQLData() throws -> PostgreSQLData {
+        var uuid = self.uuid
+        return PostgreSQLData(type: .uuid, format: .binary, data: withUnsafePointer(to: &uuid) {
+            Data(bytes: $0, count: MemoryLayout.size(ofValue: uuid))
+        })
+    }
+}
--- Tests/PostgreSQLTests/PostgreSQLConnectionTests.swift
+++ Tests/PostgreSQLTests/PostgreSQLConnectionTests.swift
@@ -126,117 +126,121 @@ class PostgreSQLConnectionTests: XCTestCase {
             XCTFail("query result count is: \(queryResult.count)")
         }
     }
-//
-//    func testParameterizedTypes() throws {
-//        let (client, eventLoop) = try PostgreSQLConnection.makeTest()
-//        let createQuery = """
-//        create table kitchen_sink (
-//            "smallint" smallint,
-//            "integer" integer,
-//            "bigint" bigint,
-//            "decimal" decimal,
-//            "numeric" numeric,
-//            "real" real,
-//            "double" double precision,
-//            "varchar" varchar(64),
-//            "char" char(4),
-//            "text" text,
-//            "bytea" bytea,
-//            "timestamp" timestamp,
-//            "date" date,
-//            "time" time,
-//            "boolean" boolean,
-//            "point" point,
-//            "uuid" uuid
-//            -- "line" line,
-//            -- "lseg" lseg,
-//            -- "box" box,
-//            -- "path" path,
-//            -- "polygon" polygon,
-//            -- "circle" circle,
-//            -- "cidr" cidr,
-//            -- "inet" inet,
-//            -- "macaddr" macaddr,
-//            -- "bit" bit(16),
-//        );
-//        """
-//        _ = try client.query("drop table if exists kitchen_sink;").await(on: eventLoop)
-//        let createResult = try client.query(createQuery).await(on: eventLoop)
-//        XCTAssertEqual(createResult.count, 0)
-//
-//        let insertQuery = """
-//        insert into kitchen_sink values (
-//            $1, -- "smallint" smallint
-//            $2, -- "integer" integer
-//            $3, -- "bigint" bigint
-//            $4::numeric, -- "decimal" decimal
-//            $5, -- "numeric" numeric
-//            $6, -- "real" real
-//            $7, -- "double" double precision
-//            $8, -- "varchar" varchar(64)
-//            $9, -- "char" char(4)
-//            $10, -- "text" text
-//            $11, -- "bytea" bytea
-//            $12, -- "timestamp" timestamp
-//            $13, -- "date" date
-//            $14, -- "time" time
-//            $15, -- "boolean" boolean
-//            $16, -- "point" point
-//            $17 -- "uuid" uuid
-//            -- "line" line,
-//            -- "lseg" lseg,
-//            -- "box" box,
-//            -- "path" path,
-//            -- "polygon" polygon,
-//            -- "circle" circle,
-//            -- "cidr" cidr,
-//            -- "inet" inet,
-//            -- "macaddr" macaddr,
-//            -- "bit" bit(16),
-//        );
-//        """
-//        let uuid = UUID()
-//        let insertResult = try client.query(insertQuery, [
-//            PostgreSQLData.int16(1), // smallint
-//            PostgreSQLData.int32(2), // integer
-//            PostgreSQLData.int64(3), // bigint
-//            PostgreSQLData.string("3.14159"), // decimal
-//            PostgreSQLData.double(5), // numeric
-//            PostgreSQLData.float(6), // real
-//            PostgreSQLData.double(7), // double
-//            PostgreSQLData.string("8"), // varchar
-//            PostgreSQLData.string("9"), // char
-//            PostgreSQLData.string("10"), // text
-//            PostgreSQLData.data(Data([0x31, 0x32])), // bytea
-//            PostgreSQLData.date(Date()), // timestamp
-//            PostgreSQLData.date(Date()), // date
-//            PostgreSQLData.date(Date()), // time
-//            PostgreSQLData.bool(true), // boolean
-//            PostgreSQLData.point(x: 11.5, y: 12), // point
-//            PostgreSQLData.uuid(uuid) // new uuid
-//        ]).await(on: eventLoop)
-//        XCTAssertEqual(insertResult.count, 0)
-//
-//        let parameterizedResult = try client.query("select * from kitchen_sink").await(on: eventLoop)
-//        if parameterizedResult.count == 1 {
-//            let row = parameterizedResult[0]
-//            XCTAssertEqual(row["smallint"], .int16(1))
-//            XCTAssertEqual(row["integer"], .int32(2))
-//            XCTAssertEqual(row["bigint"], .int64(3))
-//            XCTAssertEqual(row["decimal"], .string("3.14159"))
-//            XCTAssertEqual(row["real"], .float(6))
-//            XCTAssertEqual(row["double"], .double(7))
-//            XCTAssertEqual(row["varchar"], .string("8"))
-//            XCTAssertEqual(row["char"], .string("9   "))
-//            XCTAssertEqual(row["text"], .string("10"))
-//            XCTAssertEqual(row["bytea"], .data(Data([0x31, 0x32])))
-//            XCTAssertEqual(row["boolean"], .int8(0x01))
-//            XCTAssertEqual(row["point"], .point(x: 11.5, y: 12))
-//            XCTAssertEqual(row["uuid"], .uuid(uuid))
-//        } else {
-//            XCTFail("parameterized result count is: \(parameterizedResult.count)")
-//        }
-//    }
+
+    func testParameterizedTypes() throws {
+        let (client, eventLoop) = try PostgreSQLConnection.makeTest()
+        let createQuery = """
+        create table kitchen_sink (
+            "smallint" smallint,
+            "integer" integer,
+            "bigint" bigint,
+            "decimal" decimal,
+            "numeric" numeric,
+            "real" real,
+            "double" double precision,
+            "varchar" varchar(64),
+            "char" char(4),
+            "text" text,
+            "bytea" bytea,
+            "timestamp" timestamp,
+            "date" date,
+            "time" time,
+            "boolean" boolean,
+            "point" point,
+            "uuid" uuid
+            -- "line" line,
+            -- "lseg" lseg,
+            -- "box" box,
+            -- "path" path,
+            -- "polygon" polygon,
+            -- "circle" circle,
+            -- "cidr" cidr,
+            -- "inet" inet,
+            -- "macaddr" macaddr,
+            -- "bit" bit(16),
+        );
+        """
+        _ = try client.query("drop table if exists kitchen_sink;").await(on: eventLoop)
+        let createResult = try client.query(createQuery).await(on: eventLoop)
+        XCTAssertEqual(createResult.count, 0)
+
+        let insertQuery = """
+        insert into kitchen_sink values (
+            $1, -- "smallint" smallint
+            $2, -- "integer" integer
+            $3, -- "bigint" bigint
+            $4::numeric, -- "decimal" decimal
+            $5, -- "numeric" numeric
+            $6, -- "real" real
+            $7, -- "double" double precision
+            $8, -- "varchar" varchar(64)
+            $9, -- "char" char(4)
+            $10, -- "text" text
+            $11, -- "bytea" bytea
+            $12, -- "timestamp" timestamp
+            $13, -- "date" date
+            $14, -- "time" time
+            $15, -- "boolean" boolean
+            $16, -- "point" point
+            $17 -- "uuid" uuid
+            -- "line" line,
+            -- "lseg" lseg,
+            -- "box" box,
+            -- "path" path,
+            -- "polygon" polygon,
+            -- "circle" circle,
+            -- "cidr" cidr,
+            -- "inet" inet,
+            -- "macaddr" macaddr,
+            -- "bit" bit(16),
+        );
+        """
+
+        var params: [PostgreSQLDataCustomConvertible] = []
+        params += Int16(1) // smallint
+        params += Int32(2) // integer
+        params += Int64(3) // bigint
+        params += String("3.14159") // decimal
+        params += Double(5) // numeric
+        params += Float(6) // real
+        params += Double(7) // double
+        params += String("8") // varchar
+        params += String("9") // char
+        params += String("10") // text
+        params += Data([0x31, 0x32]) // bytea
+        params += Date() // timestamp
+        params += Date() // date
+        params += Date() // time
+        params += Bool(true) // boolean
+        params += PostgreSQLPoint(x: 11.4, y: 12) // point
+        params += UUID() // new uuid
+
+        let insertResult = try! client.query(insertQuery, params).await(on: eventLoop)
+        XCTAssertEqual(insertResult.count, 0)
+
+        let parameterizedResult = try! client.query("select * from kitchen_sink").await(on: eventLoop)
+        if parameterizedResult.count == 1 {
+            let row = parameterizedResult[0]
+            try XCTAssertEqual(row["smallint"]?.decode(Int16.self), 1)
+            try XCTAssertEqual(row["integer"]?.decode(Int32.self), 2)
+            try XCTAssertEqual(row["bigint"]?.decode(Int64.self), 3)
+            try XCTAssertEqual(row["decimal"]?.decode(String.self), "3.14159")
+            try XCTAssertEqual(row["real"]?.decode(Float.self), 6)
+            try XCTAssertEqual(row["double"]?.decode(Double.self), 7)
+            try XCTAssertEqual(row["varchar"]?.decode(String.self), "8")
+            try XCTAssertEqual(row["char"]?.decode(String.self), "9   ")
+            try XCTAssertEqual(row["text"]?.decode(String.self), "10")
+            try XCTAssertEqual(row["bytea"]?.decode(Data.self), Data([0x31, 0x32]))
+            try XCTAssertEqual(row["boolean"]?.decode(Bool.self), true)
+            try XCTAssertNotNil(row["timestamp"]?.decode(Date.self))
+            try XCTAssertNotNil(row["date"]?.decode(Date.self))
+            try XCTAssertNotNil(row["time"]?.decode(Date.self))
+            try XCTAssertEqual(row["point"]?.decode(String.self), "(11.4,12)")
+            try XCTAssertNotNil(row["uuid"]?.decode(UUID.self))
+        } else {
+            XCTFail("parameterized result count is: \(parameterizedResult.count)")
+        }
+    }
 //
 //    func testParameterizedEncodable() throws {
 //        let (client, eventLoop) = try PostgreSQLConnection.makeTest()
@@ -297,4 +301,8 @@ extension PostgreSQLConnection {
         _ = try client.authenticate(username: "postgres").await(on: eventLoop)
         return (client, eventLoop)
     }
+}
+
+func +=<T>(lhs: inout [T], rhs: T) {
+    lhs.append(rhs)
 }
