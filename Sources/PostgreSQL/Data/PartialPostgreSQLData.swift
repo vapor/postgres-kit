8ae02ac4c7edb6cd1a60432164785f5aa441987f
Tanner Nelson <me@tanner.xyz>
2018-01-19 03:05:27 -0500

    data refactor

--- Sources/PostgreSQL/Data/PartialPostgreSQLData.swift
+++ Sources/PostgreSQL/Data/PartialPostgreSQLData.swift
@@ -1,109 +1,176 @@
+import COperatingSystem
+import Foundation
+
 /// Reference wrapper for `PostgreSQLData` being mutated
 /// by the PostgreSQL data coders.
 final class PartialPostgreSQLData {
     /// The partial data.
-    var data: PostgreSQLData
+    var data: [String: PostgreSQLData]
 
     /// Creates a new `PartialPostgreSQLData`.
-    init(data: PostgreSQLData) {
+    init(data: [String: PostgreSQLData]) {
         self.data = data
     }
 
     /// Sets the `PostgreSQLData` at supplied coding path.
     func set(_ data: PostgreSQLData, at path: [CodingKey]) {
-        set(&self.data, to: data, at: path)
+        guard path.count == 1 else {
+            fatalError()
+        }
+        self.data[path[0].stringValue] = data
+        // set(&self.data, to: data, at: path)
     }
 
     /// Returns the value, if one at from the given path.
     func get(at path: [CodingKey]) -> PostgreSQLData? {
-        var child = data
-        for seg in path {
-            switch child {
-            case .array(let arr):
-                guard let index = seg.intValue, arr.count > index else {
-                    return nil
-                }
-                child = arr[index]
-            case .dictionary(let dict):
-                guard let value = dict[seg.stringValue] else {
-                    return nil
-                }
-                child = value
-            default:
-                return nil
-            }
+        guard path.count == 1 else {
+            fatalError()
         }
-        return child
+        return self.data[path[0].stringValue]
+//        var child = data
+//        for seg in path {
+//            switch child {
+//            case .array(let arr):
+//                guard let index = seg.intValue, arr.count > index else {
+//                    return nil
+//                }
+//                child = arr[index]
+//            case .dictionary(let dict):
+//                guard let value = dict[seg.stringValue] else {
+//                    return nil
+//                }
+//                child = value
+//            default:
+//                return nil
+//            }
+//        }
+//        return child
     }
 
-    /// Sets the mutable `PostgreSQLData` to supplied data at coding path.
-    private func set(_ context: inout PostgreSQLData, to value: PostgreSQLData, at path: [CodingKey]) {
-        guard path.count >= 1 else {
-            context = value
-            return
-        }
-
-        let end = path[0]
-
-        var child: PostgreSQLData?
-        switch path.count {
-        case 1:
-            child = value
-        case 2...:
-            if let index = end.intValue {
-                let array = context.array ?? []
-                if array.count > index {
-                    child = array[index]
-                } else {
-                    child = PostgreSQLData.array([])
-                }
-                set(&child!, to: value, at: Array(path[1...]))
-            } else {
-                child = context.dictionary?[end.stringValue] ?? PostgreSQLData.dictionary([:])
-                set(&child!, to: value, at: Array(path[1...]))
-            }
-        default: break
-        }
-
-        if let index = end.intValue {
-            if case .array(var arr) = context {
-                if arr.count > index {
-                    arr[index] = child ?? .null
-                } else {
-                    arr.append(child ?? .null)
-                }
-                context = .array(arr)
-            } else if let child = child {
-                context = .array([child])
-            }
-        } else {
-            if case .dictionary(var dict) = context {
-                dict[end.stringValue] = child
-                context = .dictionary(dict)
-            } else if let child = child {
-                context = .dictionary([
-                    end.stringValue: child
-                ])
-            }
-        }
-    }
+//    /// Sets the mutable `PostgreSQLData` to supplied data at coding path.
+//    private func set(_ context: inout PostgreSQLData, to value: PostgreSQLData, at path: [CodingKey]) {
+//        guard path.count >= 1 else {
+//            context = value
+//            return
+//        }
+//
+//        let end = path[0]
+//
+//        var child: PostgreSQLData?
+//        switch path.count {
+//        case 1:
+//            child = value
+//        case 2...:
+//            if let index = end.intValue {
+//                let array = context.array ?? []
+//                if array.count > index {
+//                    child = array[index]
+//                } else {
+//                    child = PostgreSQLData.array([])
+//                }
+//                set(&child!, to: value, at: Array(path[1...]))
+//            } else {
+//                child = context.dictionary?[end.stringValue] ?? PostgreSQLData.dictionary([:])
+//                set(&child!, to: value, at: Array(path[1...]))
+//            }
+//        default: break
+//        }
+//
+//        if let index = end.intValue {
+//            if case .array(var arr) = context {
+//                if arr.count > index {
+//                    arr[index] = child ?? .null
+//                } else {
+//                    arr.append(child ?? .null)
+//                }
+//                context = .array(arr)
+//            } else if let child = child {
+//                context = .array([child])
+//            }
+//        } else {
+//            if case .dictionary(var dict) = context {
+//                dict[end.stringValue] = child
+//                context = .dictionary(dict)
+//            } else if let child = child {
+//                context = .dictionary([
+//                    end.stringValue: child
+//                ])
+//            }
+//        }
+//    }
 }
 
 
 /// MARK: Encoding Convenience
+
+extension FixedWidthInteger {
+    /// Big-endian bytes for this integer.
+    fileprivate var data: Data {
+        var bytes = [UInt8](repeating: 0, count: Self.bitWidth / 8)
+        var intNetwork = bigEndian
+        memcpy(&bytes, &intNetwork, bytes.count)
+        return Data(bytes)
+    }
+}
+
+extension FloatingPoint {
+    /// Big-endian bytes for this floating-point number.
+    fileprivate var data: Data {
+        var bytes = [UInt8](repeating: 0, count: MemoryLayout<Self>.size)
+        var copy = self
+        memcpy(&bytes, &copy, bytes.count)
+        return Data(bytes.reversed())
+    }
+}
+
+extension Data {
+    /// Converts this data to a floating-point number.
+    fileprivate func makeFloatingPoint<F>(_ type: F.Type = F.self) -> F where F: FloatingPoint {
+        return Data(reversed()).unsafeCast()
+    }
+
+    /// Converts this data to a fixed-width integer.
+    fileprivate func makeFixedWidthInteger<I>(_ type: I.Type = I.self) -> I where I: FixedWidthInteger {
+        return unsafeCast(to: I.self).bigEndian
+    }
+
+    fileprivate func unsafeCast<T>(to type: T.Type = T.self) -> T {
+        return withUnsafeBytes { (pointer: UnsafePointer<T>) -> T in
+            return pointer.pointee
+        }
+    }
+
+    /// Convert the row's data into a string, throwing if invalid encoding.
+    fileprivate func makeString(encoding: String.Encoding = .utf8) throws -> String {
+        guard let string = String(data: self, encoding: encoding) else {
+            throw PostgreSQLError(identifier: "utf8String", reason: "Unexpected non-UTF8 string.")
+        }
+
+        return string
+    }
+}
 
 extension PartialPostgreSQLData {
     /// Sets a generic fixed width integer to the supplied path.
     func setFixedWidthInteger<U>(_ value: U, at path: [CodingKey]) throws
         where U: FixedWidthInteger
     {
+//        switch U.bitWidth {
+//        case 8: try set(.int8(safeCast(value, at: path)), at: path)
+//        case 16: try set(.int16(safeCast(value, at: path)), at: path)
+//        case 32: try set(.int32(safeCast(value, at: path)), at: path)
+//        case 64: try set(.int64(safeCast(value, at: path)), at: path)
+//        default: throw DecodingError.typeMismatch(U.self, .init(codingPath: path, debugDescription: "Integer bit width not supported: \(U.bitWidth)"))
+//        }
+        let type: PostgreSQLDataType
         switch U.bitWidth {
-        case 8: try set(.int8(safeCast(value, at: path)), at: path)
-        case 16: try set(.int16(safeCast(value, at: path)), at: path)
-        case 32: try set(.int32(safeCast(value, at: path)), at: path)
-        case 64: try set(.int64(safeCast(value, at: path)), at: path)
+        case 8: type = .char
+        case 16: type = .int2
+        case 32: type = .int4
+        case 64: type = .int8
         default: throw DecodingError.typeMismatch(U.self, .init(codingPath: path, debugDescription: "Integer bit width not supported: \(U.bitWidth)"))
         }
+        set(.init(type: type, data: value.data), at: path)
     }
 
     /// Sets an encodable value at the supplied path.
@@ -142,14 +209,18 @@ extension PartialPostgreSQLData {
     }
 
     /// Gets a `Float` from the supplied path or throws a decoding error.
-    func requireFixedWidthItenger<I>(_ type: I.Type = I.self, at path: [CodingKey]) throws -> I
+    func requireFixedWidthInteger<I>(_ type: I.Type = I.self, at path: [CodingKey]) throws -> I
         where I: FixedWidthInteger
     {
-        switch try requireGet(I.self, at: path) {
-        case .int8(let value): return try safeCast(value, at: path)
-        case .int16(let value): return try safeCast(value, at: path)
-        case .int32(let value): return try safeCast(value, at: path)
-        case .int64(let value): return try safeCast(value, at: path)
+        let data = try requireGet(I.self, at: path)
+        guard let value = data.data else {
+            fatalError()
+        }
+        switch data.type {
+        case .char: return try safeCast(value.makeFixedWidthInteger(Int8.self), at: path)
+        case .int2: return try safeCast(value.makeFixedWidthInteger(Int16.self), at: path)
+        case .int4: return try safeCast(value.makeFixedWidthInteger(Int32.self), at: path)
+        case .int8: return try safeCast(value.makeFixedWidthInteger(Int64.self), at: path)
         default: throw DecodingError.typeMismatch(type, .init(codingPath: path, debugDescription: ""))
         }
     }
@@ -176,29 +247,41 @@ extension PartialPostgreSQLData {
     func requireFloatingPoint<F>(_ type: F.Type = F.self, at path: [CodingKey]) throws -> F
         where F: BinaryFloatingPoint
     {
-        switch try requireGet(F.self, at: path) {
-        case .int8(let value): return F(value)
-        case .int16(let value): return F(value)
-        case .int32(let value): return F(value)
-        case .int64(let value): return F(value)
-        case .float(let value): return F(value)
-        case .double(let value): return F(value)
+        let data = try requireGet(F.self, at: path)
+        guard let value = data.data else {
+            fatalError()
+        }
+        switch data.type {
+        case .char: return F(value.makeFixedWidthInteger(Int8.self))
+        case .int2: return F(value.makeFixedWidthInteger(Int16.self))
+        case .int4: return F(value.makeFixedWidthInteger(Int32.self))
+        case .int8: return F(value.makeFixedWidthInteger(Int64.self))
+        case .float4: return F(value.makeFloatingPoint(Float.self))
+        case .float8: return F(value.makeFloatingPoint(Double.self))
         default: throw DecodingError.typeMismatch(F.self, .init(codingPath: path, debugDescription: ""))
         }
     }
 
     /// Gets a `String` from the supplied path or throws a decoding error.
     func requireString(at path: [CodingKey]) throws -> String {
-        switch try requireGet(String.self, at: path) {
-        case .string(let value): return value
+        let data = try requireGet(String.self, at: path)
+        guard let value = data.data else {
+            fatalError()
+        }
+        switch data.type {
+        case .text: return String(data: value, encoding: .utf8) !! "Non-utf8"
         default: throw DecodingError.typeMismatch(String.self, .init(codingPath: path, debugDescription: ""))
         }
     }
 
     /// Gets a `Bool` from the supplied path or throws a decoding error.
     func requireBool(at path: [CodingKey]) throws -> Bool {
-        switch try requireGet(Bool.self, at: path) {
-        case .bool(let value): return value
+        let data = try requireGet(String.self, at: path)
+        guard let value = data.data else {
+            fatalError()
+        }
+        switch data.type {
+        case .char: return value.makeFixedWidthInteger(Int8.self) == 1
         default: throw DecodingError.typeMismatch(Bool.self, .init(codingPath: path, debugDescription: ""))
         }
     }
