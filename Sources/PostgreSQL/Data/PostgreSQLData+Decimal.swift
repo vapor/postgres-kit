390c22ce049c0565ec4b93d764511191eb9c4dd7
tanner0101 <me@tanner.xyz>
2018-03-28 21:44:11 -0400

    add decimal support

---
+++ Sources/PostgreSQL/Data/PostgreSQLData+Decimal.swift
@@ -0,0 +1,23 @@
+import Core
+
+extension Decimal: PostgreSQLDataConvertible {
+    /// See `PostgreSQLDataConvertible`
+    public static var postgreSQLDataType: PostgreSQLDataType {
+        return Double.postgreSQLDataType
+    }
+
+    /// See `PostgreSQLDataConvertible`
+    public static var postgreSQLDataArrayType: PostgreSQLDataType {
+        return Double.postgreSQLDataArrayType
+    }
+
+    /// See `PostgreSQLDataConvertible`
+    public static func convertFromPostgreSQLData(_ data: PostgreSQLData) throws -> Decimal {
+        return try Decimal(Double.convertFromPostgreSQLData(data))
+    }
+
+    /// See `PostgreSQLDataConvertible`
+    public func convertToPostgreSQLData() throws -> PostgreSQLData {
+        return try Double(description).convertToPostgreSQLData()
+    }
+}
