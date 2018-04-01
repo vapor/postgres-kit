c23e8b1155729886f5d7a24158d52536bc247850
MAYUR MAHAJAN <mayur.mahajan@icloud.com>
2018-03-31 19:35:25 -0700

    Fix database name parsing issue with URL initialization of postgres config

--- Sources/PostgreSQL/Database/PostgreSQLDatabaseConfig.swift
+++ Sources/PostgreSQL/Database/PostgreSQLDatabaseConfig.swift
@@ -3,7 +3,7 @@ import Foundation
 public struct PostgreSQLDatabaseConfig {
     /// Creates a `PostgreSQLDatabaseConfig` with default settings.
     public static func `default`() -> PostgreSQLDatabaseConfig {
-        return .init(hostname: "localhost", port: 5432, username: "postgres")
+        return .init(hostname: "localhost", port: nil, username: "postgres")
     }
 
     /// Destination hostname.
@@ -23,9 +23,9 @@ public struct PostgreSQLDatabaseConfig {
     public let password: String?
     
     /// Creates a new `PostgreSQLDatabaseConfig`.
-    public init(hostname: String, port: Int = 5432, username: String, database: String? = nil, password: String? = nil) {
+    public init(hostname: String, port: Int?, username: String, database: String? = nil, password: String? = nil) {
         self.hostname = hostname
-        self.port = port
+        self.port = port ?? 5432
         self.username = username
         self.database = database
         self.password = password
@@ -34,22 +34,18 @@ public struct PostgreSQLDatabaseConfig {
     /// Creates a `PostgreSQLDatabaseConfig` frome a connection string.
     public init(url: String) throws {
         guard let urL = URL(string: url),
-            let hostname = urL.host,
-            let port = urL.port,
             let username = urL.user,
-            let database = URL(string: url)?.path,
-            database.count > 0
+			let hostname = urL.host,
+            let path = URL(string: url)?.path,
+			"postgres" == urL.scheme
              else {
                 throw PostgreSQLError(identifier: "Bad Connection String",
-                                 reason: "Host could not be parsed",
+                                 reason: "Config could not be parsed",
                                  possibleCauses: ["Foundation URL is unable to parse the provided connection string"],
                                  suggestedFixes: ["Check the connection string being passed"],
                                  source: .capture())
         }
-        self.hostname = hostname
-        self.port = port
-        self.username = username
-        self.database = database
-        self.password = urL.password
+		let database = String(path.dropFirst())
+		self.init(hostname: hostname, port: urL.port, username: username, database: database, password: urL.password)
     }
 }
