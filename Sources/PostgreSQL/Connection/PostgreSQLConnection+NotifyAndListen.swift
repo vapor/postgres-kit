b4ca67f42f0639413d74b7a2b4bfcc2d63337a34
Shaun Hubbard <shaun@splife.com>
2018-04-25 14:41:40 -0400

    fixed potential issue with case insensivitiy, @MrMage brought it to my attention

--- Sources/PostgreSQL/Connection/PostgreSQLConnection+NotifyAndListen.swift
+++ Sources/PostgreSQL/Connection/PostgreSQLConnection+NotifyAndListen.swift
@@ -6,10 +6,10 @@ extension PostgreSQLConnection {
         handler: @escaping (String) throws -> ()
         ) throws -> Future<Void> {
         beforeClose = { conn in
-            let query = PostgreSQLQuery(query: "UNLISTEN \(channelName);")
+            let query = PostgreSQLQuery(query: "UNLISTEN \"\(channelName)\";")
             return conn.send([.query(query)], onResponse: { _ in })
         }
-        let query = PostgreSQLQuery(query: "LISTEN \(channelName);")
+        let query = PostgreSQLQuery(query: "LISTEN \"\(channelName)\";")
         return queue.enqueue([.query(query)], onInput: { message in
             switch message {
             case let .notificationResponse(notification):
@@ -23,7 +23,7 @@ extension PostgreSQLConnection {
 
     public func notify(
         _ channelName: String, message: String) throws -> Future<Void> {
-        let query = PostgreSQLQuery(query: "NOTIFY \(channelName), '\(message)';")
+        let query = PostgreSQLQuery(query: "NOTIFY \"\(channelName)\", '\(message)';")
         return send([.query(query)]).map(to: Void.self, { _ in })
     }
 }
