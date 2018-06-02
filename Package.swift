3b977c0947f11d2a002545d870ffb6d861191e69
tanner0101 <me@tanner.xyz>
2018-06-02 01:04:00 -0400

    use fluent gm branch for circle ci

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
         .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0"),
 
         // 🗄 Core services for creating database integrations.
-        .package(url: "https://github.com/vapor/database-kit.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/database-kit.git", .branch("fluent-gm")),
 
         // 📦 Dependency injection / inversion of control framework.
         .package(url: "https://github.com/vapor/service.git", from: "1.0.0"),
 
         // *️⃣ Build SQL queries in Swift.
-        .package(url: "https://github.com/vapor/sql.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/sql.git", .branch("fluent-gm")),
         
         // Event-driven network application framework for high performance protocol servers & clients, non-blocking.
         .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
