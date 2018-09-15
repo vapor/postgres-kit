e3853d1c347ae436c6103aaf87e305afa7ee2098
Danni Stjernegård <stjernegard@me.com>
2018-09-15 12:39:59 +0200

    Remove .init(op:) in favor of ExpressibleByStringLiteral

--- Sources/PostgreSQL/SQL/PostgreSQLBinaryOperator.swift
+++ Sources/PostgreSQL/SQL/PostgreSQLBinaryOperator.swift
@@ -1,109 +1,104 @@
 /// PostgreSQL specific `SQLBinaryOperator`.
 public struct PostgreSQLBinaryOperator: SQLBinaryOperator, Equatable, ExpressibleByStringLiteral {
     /// See `SQLBinaryOperator`.
-    public static let add = PostgreSQLBinaryOperator("+")
+    public static let add: PostgreSQLBinaryOperator = "+"
 
     /// See `SQLBinaryOperator`.
-    public static let bitwiseAnd = PostgreSQLBinaryOperator("&")
+    public static let bitwiseAnd: PostgreSQLBinaryOperator = "&"
 
     /// See `SQLBinaryOperator`.
-    public static let bitwiseOr = PostgreSQLBinaryOperator("|")
+    public static let bitwiseOr: PostgreSQLBinaryOperator = "|"
 
     /// See `SQLBinaryOperator`.
-    public static let bitwiseShiftLeft = PostgreSQLBinaryOperator("<<")
+    public static let bitwiseShiftLeft: PostgreSQLBinaryOperator = "<<"
 
     /// See `SQLBinaryOperator`.
-    public static let bitwiseShiftRight = PostgreSQLBinaryOperator(">>")
+    public static let bitwiseShiftRight: PostgreSQLBinaryOperator = ">>"
 
     /// See `SQLBinaryOperator`.
-    public static let concatenate = PostgreSQLBinaryOperator("||")
+    public static let concatenate: PostgreSQLBinaryOperator = "||"
 
     /// See `SQLBinaryOperator`.
-    public static let divide = PostgreSQLBinaryOperator("/")
+    public static let divide: PostgreSQLBinaryOperator = "/"
 
     /// See `SQLBinaryOperator`.
-    public static let equal = PostgreSQLBinaryOperator("=")
+    public static let equal: PostgreSQLBinaryOperator = "="
 
     /// See `SQLBinaryOperator`.
-    public static let greaterThan = PostgreSQLBinaryOperator(">")
+    public static let greaterThan: PostgreSQLBinaryOperator = ">"
 
     /// See `SQLBinaryOperator`.
-    public static let greaterThanOrEqual = PostgreSQLBinaryOperator(">=")
+    public static let greaterThanOrEqual: PostgreSQLBinaryOperator = ">="
 
     /// See `SQLBinaryOperator`.
-    public static let lessThan = PostgreSQLBinaryOperator("<")
+    public static let lessThan: PostgreSQLBinaryOperator = "<"
 
     /// See `SQLBinaryOperator`.
-    public static let lessThanOrEqual = PostgreSQLBinaryOperator("<=")
+    public static let lessThanOrEqual: PostgreSQLBinaryOperator = "<="
 
     /// See `SQLBinaryOperator`.
-    public static let modulo = PostgreSQLBinaryOperator("%")
+    public static let modulo: PostgreSQLBinaryOperator = "%"
 
     /// See `SQLBinaryOperator`.
-    public static let multiply = PostgreSQLBinaryOperator("*")
+    public static let multiply: PostgreSQLBinaryOperator = "*"
 
     /// See `SQLBinaryOperator`.
-    public static let notEqual = PostgreSQLBinaryOperator("!=")
+    public static let notEqual: PostgreSQLBinaryOperator = "!="
 
     /// See `SQLBinaryOperator`.
-    public static let subtract = PostgreSQLBinaryOperator("-")
+    public static let subtract: PostgreSQLBinaryOperator = "-"
 
     /// See `SQLBinaryOperator`.
-    public static let and = PostgreSQLBinaryOperator("AND")
+    public static let and: PostgreSQLBinaryOperator = "AND"
 
     /// See `SQLBinaryOperator`.
-    public static let or = PostgreSQLBinaryOperator("OR")
+    public static let or: PostgreSQLBinaryOperator = "OR"
 
     /// See `SQLBinaryOperator`.
-    public static let `in` = PostgreSQLBinaryOperator("IN")
+    public static let `in`: PostgreSQLBinaryOperator = "IN"
 
     /// See `SQLBinaryOperator`.
-    public static let notIn = PostgreSQLBinaryOperator("NOT IN")
+    public static let notIn: PostgreSQLBinaryOperator = "NOT IN"
 
     /// See `SQLBinaryOperator`.
-    public static let `is` = PostgreSQLBinaryOperator("IS")
+    public static let `is`: PostgreSQLBinaryOperator = "IS"
 
     /// See `SQLBinaryOperator`.
-    public static let isNot = PostgreSQLBinaryOperator("IS NOT")
+    public static let isNot: PostgreSQLBinaryOperator = "IS NOT"
 
     /// See `SQLBinaryOperator`.
-    public static let like = PostgreSQLBinaryOperator("LIKE")
+    public static let like: PostgreSQLBinaryOperator = "LIKE"
 
     /// See `SQLBinaryOperator`.
-    public static let glob = PostgreSQLBinaryOperator("GLOB")
+    public static let glob: PostgreSQLBinaryOperator = "GLOB"
 
     /// See `SQLBinaryOperator`.
-    public static let match = PostgreSQLBinaryOperator("MATCH")
+    public static let match: PostgreSQLBinaryOperator = "MATCH"
 
     /// See `SQLBinaryOperator`.
-    public static let regexp = PostgreSQLBinaryOperator("~")
+    public static let regexp: PostgreSQLBinaryOperator = "~"
 
     /// See `SQLBinaryOperator`.
-    public static let notLike = PostgreSQLBinaryOperator("NOT LIKE")
+    public static let notLike: PostgreSQLBinaryOperator = "NOT LIKE"
 
     /// See `SQLBinaryOperator`.
-    public static let notGlob = PostgreSQLBinaryOperator("NOT GLOB")
+    public static let notGlob: PostgreSQLBinaryOperator = "NOT GLOB"
 
     /// See `SQLBinaryOperator`.
-    public static let notMatch = PostgreSQLBinaryOperator("NOT MATCH")
+    public static let notMatch: PostgreSQLBinaryOperator = "NOT MATCH"
 
     /// See `SQLBinaryOperator`.
-    public static let notRegexp = PostgreSQLBinaryOperator("NOT REGEXP")
+    public static let notRegexp: PostgreSQLBinaryOperator = "NOT REGEXP"
 
     /// See `SQLBinaryOperator`.
-    public static let ilike = PostgreSQLBinaryOperator("ILIKE")
+    public static let ilike: PostgreSQLBinaryOperator = "ILIKE"
 
     /// See `SQLBinaryOperator`.
-    public static let notILike = PostgreSQLBinaryOperator("NOT ILIKE")
+    public static let notILike: PostgreSQLBinaryOperator = "NOT ILIKE"
 
-    public let op: String
-
-    public init(_ op: String) {
-        self.op = op
-    }
-
+    private let op: String
     public init(stringLiteral value: String) {
-        self.init(value)
+        self.op = value
     }
 
     public func serialize(_ binds: inout [Encodable]) -> String {
