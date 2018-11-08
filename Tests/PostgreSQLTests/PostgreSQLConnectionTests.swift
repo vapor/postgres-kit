92881d8b29b7fef572b0d3d56b71527e8a4baeca
Tanner Nelson <me@tanner.xyz>
2018-11-07 19:22:35 -0500

    add PostgresDataEncoder

--- Tests/PostgreSQLTests/PostgreSQLConnectionTests.swift
+++ Tests/PostgreSQLTests/PostgreSQLConnectionTests.swift
@@ -588,6 +588,24 @@ class PostgreSQLConnectionTests: XCTestCase {
             XCTAssert(result == Double(value))
         }
     }
+    
+    func testRangeSelectDecodePerformance() throws {
+        struct Series: Decodable {
+            var num: Int
+        }
+        
+        let conn = try PostgreSQLConnection.makeTest()
+        measure {
+            let decoder = PostgreSQLRowDecoder()
+            do {
+                try conn.simpleQuery("SELECT * FROM generate_series(1, 10000) num") { row in
+                    _ = try decoder.decode(Series.self, from: row)
+                }.wait()
+            } catch {
+                XCTFail("\(error)")
+            }
+        }
+    }
 
     static var allTests = [
         ("testBenchmark", testBenchmark),
