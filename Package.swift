45784349eb1646db0d34b66157332ac795ec0886
Tanner <tannernelson@gmail.com>
2019-12-09 14:13:41 -0500

    beta.2 (#158)

--- Package.swift
+++ Package.swift
@@ -10,9 +10,9 @@ let package = Package(
         .library(name: "PostgresKit", targets: ["PostgresKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/postgres-nio.git", .branch("master")),
-        .package(url: "https://github.com/vapor/sql-kit.git", .branch("master")),
-        .package(url: "https://github.com/vapor/async-kit.git", .branch("master")),
+        .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0-beta.2"),
     ],
     targets: [
         .target(name: "PostgresKit", dependencies: ["AsyncKit", "PostgresNIO", "SQLKit"]),
--- Tests/LinuxMain.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-
-import PostgresKitTests
-
-var tests = [XCTestCaseEntry]()
-tests += PostgresKitTests.__allTests()
-
-XCTMain(tests)
