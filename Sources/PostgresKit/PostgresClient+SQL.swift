388dec1aa4a7bdf879ffd3900cd65be406a9e62b
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-12-11 07:41:46 -0600

    Replace uses of PostgresRow.sqlRow with .sql

--- Sources/PostgresKit/PostgresClient+SQL.swift
+++ Sources/PostgresKit/PostgresClient+SQL.swift
@@ -36,7 +36,7 @@ extension _PostgresSQLDatabase: SQLDatabase {
             return try self.database.query(sql, binds.map { encodable in
                 return try self.encoder.encode(encodable)
             }) { row in
-                onRow(row.sqlRow(using: self.decoder))
+                onRow(row.sql(using: self.decoder))
             }
         } catch {
             return self.eventLoop.makeFailedFuture(error)
