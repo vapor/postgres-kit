b6867c73727d81b8501e57f2259516841ac36d0d
Gwynne Raskind <gwynne@vapor.codes>
2022-03-18 14:07:26 -0500

    Generating NIOSSLContexts is (very) expensive, do it only once (per connection source, at least) rather than for every connection.

--- Sources/PostgresKit/PostgresConnectionSource.swift
+++ Sources/PostgresKit/PostgresConnectionSource.swift
@@ -1,11 +1,15 @@
 import NIOConcurrencyHelpers
+import NIOSSL
 
 public struct PostgresConnectionSource: ConnectionPoolSource {
     public let configuration: PostgresConfiguration
+    public let sslContext: Result<NIOSSLContext?, Error>
     private static let idGenerator = NIOAtomic.makeAtomic(value: 0)
 
     public init(configuration: PostgresConfiguration) {
         self.configuration = configuration
+        // TODO: Figure out a way to throw errors from this initializer sensibly, or to lazily init the NIOSSLContext only once in makeConnection()
+        self.sslContext = .init(catching: { try configuration._hostname.flatMap { _ in try configuration.tlsConfiguration.map { try .init(configuration: $0) } } })
     }
 
     public func makeConnection(
@@ -13,12 +17,17 @@ public struct PostgresConnectionSource: ConnectionPoolSource {
         on eventLoop: EventLoop
     ) -> EventLoopFuture<PostgresConnection> {
         if let hostname = self.configuration._hostname {
+            let tlsMode: PostgresConnection.Configuration.TLS
+            switch self.sslContext {
+                case let .success(sslContext): tlsMode = sslContext.map { .require($0) } ?? .disable
+                case let .failure(error): return eventLoop.makeFailedFuture(error)
+            }
             let future = PostgresConnection.connect(
                 on: eventLoop,
                 configuration: .init(
                     connection: .init(host: hostname, port: self.configuration._port ?? PostgresConfiguration.ianaPortNumber),
                     authentication: .init(username: self.configuration.username, database: self.configuration.database, password: self.configuration.password),
-                    tls: self.configuration.tlsConfiguration.map { .require(try! .init(configuration: $0)) } ?? .disable
+                    tls: tlsMode
                 ),
                 id: Self.idGenerator.add(1),
                 logger: logger
