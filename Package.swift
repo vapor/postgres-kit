1174d9bc57798aba7a99451e5380c0eb0fb796d8
Gwynne Raskind <gwynne@vapor.codes>
2022-11-26 20:27:27 -0600

    Update minimum Swift version to 5.5 (#236)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
@@ -10,9 +10,9 @@ let package = Package(
         .library(name: "PostgresKit", targets: ["PostgresKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.11.0"),
-        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.16.0"),
-        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.12.0"),
+        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.22.0"),
+        .package(url: "https://github.com/vapor/async-kit.git", from: "1.14.0"),
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.0")
     ],
     targets: [
