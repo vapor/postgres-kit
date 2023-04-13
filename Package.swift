dae06f72f37b86dd52873db1b8e6e255e7190b25
Gwynne Raskind <gwynne@vapor.codes>
2023-04-13 14:45:53 -0500

    Update minimum Swift version to 5.6 and make platform versions consistent.

--- Package.swift
+++ Package.swift
@@ -1,12 +1,13 @@
-// swift-tools-version:5.5.2
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "postgres-kit",
     platforms: [
-       .macOS(.v10_15),
-       .iOS(.v13),
-       .watchOS(.v6)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
     ],
     products: [
         .library(name: "PostgresKit", targets: ["PostgresKit"]),
