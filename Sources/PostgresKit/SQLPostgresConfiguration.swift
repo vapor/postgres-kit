15d7168d35818a81439179e3e7c10362f89bb1a3
Gwynne Raskind <gwynne@vapor.codes>
2023-07-27 11:00:49 -0500

    Add support for the sslmode values accepted by libpq, and support for UDS URLs.

--- Sources/PostgresKit/SQLPostgresConfiguration.swift
+++ Sources/PostgresKit/SQLPostgresConfiguration.swift
@@ -27,40 +27,93 @@ public struct SQLPostgresConfiguration {
     
     /// Create a ``SQLPostgresConfiguration`` from a properly formatted URL.
     ///
-    /// The allowed URL format is:
+    /// The supported URL formats are:
     ///
-    ///     postgres://username:password@hostname:port/database?tls=mode
+    ///     postgres://username:password@hostname:port/database?tlsmode=mode
+    ///     postgres+tcp://username:password@hostname:port/database?tlsmode=mode
+    ///     postgres+uds://username:password@localhost/path?tlsmode=mode#database
     ///
-    /// `hostname` and `username` are required; all other components are optional. For backwards
-    /// compatibility, `ssl` is treated as an alias of `tls`.
+    /// The `postgres+tcp` scheme requests a connection over TCP. The `postgres` scheme is an alias
+    /// for `postgres+tcp`. Only the `hostname` and `username` components are required.
     ///
-    /// The allowed `mode` values for `tls` are:
-    ///   - `require` (fail to connect if the server does not support TLS)
-    ///   - `true` (attempt to use TLS but continue anyway if the server doesn't support it)
-    ///   - `false` (do not use TLS even if the server supports it).
-    /// If `tls` is omitted entirely, the mode defaults to `true`.
+    /// The `postgres+uds` scheme requests a connection via a UNIX domain socket. The `username` and
+    /// `path` components are required. The authority must always be empty or `localhost`, and may not
+    /// specify a port.
+    ///
+    /// The allowed `mode` values for `tlsmode` are:
+    ///
+    /// Value|Behavior
+    /// -|-
+    /// `disable`|Don't use TLS, even if the server supports it.
+    /// `prefer`|Use TLS if possible.
+    /// `require`|Enforce TLS support.
+    ///
+    /// If no `tlsmode` is specified, the default mode is `prefer` for TCP connections, or `disable`
+    /// for UDS connections. If more than one mode is specified, the last one wins. Whenever a TLS
+    /// connection is made, full certificate verification (both chain of trust and hostname match)
+    /// is always enforced, regardless of the mode used.
+    ///
+    /// For compatibility with `libpq` and previous versions of this package, any of "`sslmode`",
+    /// "`tls`", or "`ssl`" may be used instead of "`tlsmode`". There are also various aliases for
+    /// each of the TLS mode names, as follows:
+    ///
+    /// - "`disable`": "`false`"
+    /// - "`prefer`": "`allow`", "`true`"
+    /// - "`require`": "`verify-ca`", "`verify-full`"
+    ///
+    /// The aliases always have the same semantics as the "canonical" modes, despite any differences
+    /// suggested by their names.
+    ///
+    /// > Note: It is possible to emulate `libpq`'s definitions for `prefer` (TLS if available with
+    /// > no certificate verification), `require` (TLS enforced, but also without certificate
+    /// > verification) and `verify-ca` (TLS enforced with no hostname verification) by manually
+    /// > specifying the TLS configuration instead of using a URL. It is not possible, by design, to
+    /// > emulate `libpq`'s `allow` mode (TLS only if there is no alternative). It is _strongly_
+    /// > recommended for both security and privacy reasons to always leave full certificate
+    /// > verification enabled whenever possible. See NIOSSL's [`TLSConfiguration`](tlsconfig) for
+    /// > additional information and recommendations.
+    ///
+    /// [tlsconfig]:
+    /// https://swiftpackageindex.com/apple/swift-nio-ssl/main/documentation/niossl/tlsconfiguration
     public init(url: URL) throws {
-        guard let comp = URLComponents(url: url, resolvingAgainstBaseURL: true),
-              comp.scheme?.hasPrefix("postgres") ?? false,
-              let hostname = comp.host, let username = comp.user
-        else {
+        guard let comp = URLComponents(url: url, resolvingAgainstBaseURL: true), let username = comp.user else {
             throw URLError(.badURL, userInfo: [NSURLErrorFailingURLErrorKey: url, NSURLErrorFailingURLStringErrorKey: url.absoluteString])
         }
-        let password = comp.password, port = comp.port ?? Self.ianaPortNumber
-        let tls: PostgresConnection.Configuration.TLS
-        switch (comp.queryItems ?? []).first(where: { ["ssl", "tls"].contains($0.name.lowercased()) })?.value ?? "true" {
-        case "require": tls = try .require(.init(configuration: .makeClientConfiguration()))
-        case "true":  tls = try .prefer(.init(configuration: .makeClientConfiguration()))
-        case "false": tls = .disable
-        default: throw URLError(.badURL, userInfo: [NSURLErrorFailingURLErrorKey: url, NSURLErrorFailingURLStringErrorKey: url.absoluteString])
+        
+        func decideTLSConfig(from queryItems: [URLQueryItem], defaultMode: String) throws -> PostgresConnection.Configuration.TLS {
+            switch queryItems.last(where: { ["tlsmode", "sslmode", "ssl", "tls"].contains($0.name.lowercased()) })?.value ?? defaultMode {
+            case "verify-full", "verify-ca", "require":
+                return try .require(.init(configuration: .makeClientConfiguration()))
+            case "prefer", "allow", "true":
+                return try .prefer(.init(configuration: .makeClientConfiguration()))
+            case "disable", "false":
+                return .disable
+            default:
+                throw URLError(.badURL, userInfo: [NSURLErrorFailingURLErrorKey: url, NSURLErrorFailingURLStringErrorKey: url.absoluteString])
+            }
         }
         
-        self.init(
-            hostname: hostname, port: port,
-            username: username, password: password,
-            database: url.lastPathComponent,
-            tls: tls
-        )
+        switch comp.scheme {
+        case "postgres", "postgres+tcp":
+            guard let hostname = comp.host, !hostname.isEmpty else {
+                throw URLError(.badURL, userInfo: [NSURLErrorFailingURLErrorKey: url, NSURLErrorFailingURLStringErrorKey: url.absoluteString])
+            }
+            self.init(
+                hostname: hostname, port: comp.port ?? Self.ianaPortNumber,
+                username: username, password: comp.password,
+                database: url.lastPathComponent.isEmpty ? nil : url.lastPathComponent,
+                tls: try decideTLSConfig(from: comp.queryItems ?? [], defaultMode: "prefer")
+            )
+        case "postgres+uds":
+            guard (comp.host?.isEmpty ?? true || comp.host == "localhost"), comp.port == nil, !comp.path.isEmpty, comp.path != "/" else {
+                throw URLError(.badURL, userInfo: [NSURLErrorFailingURLErrorKey: url, NSURLErrorFailingURLStringErrorKey: url.absoluteString])
+            }
+            var coreConfig = PostgresConnection.Configuration(unixSocketPath: comp.path, username: username, password: comp.password, database: comp.fragment)
+            coreConfig.tls = try decideTLSConfig(from: comp.queryItems ?? [], defaultMode: "disable")
+            self.init(coreConfiguration: coreConfig)
+        default:
+            throw URLError(.badURL, userInfo: [NSURLErrorFailingURLErrorKey: url, NSURLErrorFailingURLStringErrorKey: url.absoluteString])
+        }
     }
     
     /// Create a ``SQLPostgresConfiguration`` for connecting to a server with a hostname and optional port.
